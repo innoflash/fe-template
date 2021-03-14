aca70ee3d6e21f84554496829763c19c8eaec59f
innoflash <innocentmazando@gmail.com>
2021-03-14 15:35:14 +0200

    added http modules

---
+++ src/app/shared/abstracts/loads-paginated.data.ts
@@ -0,0 +1,18 @@
+import { Directive } from '@angular/core';
+import { Meta } from '@fe-template/shared/models/responses/meta';
+
+@Directive()
+// tslint:disable-next-line:directive-class-suffix
+export class LoadsPaginatedData {
+  public currentPage = 1;
+  public perPage = 1;
+  public total = 0;
+  protected path = '';
+
+  protected initPagination(meta: Meta): void {
+    this.currentPage = meta.current_page;
+    this.total = meta.total;
+    this.perPage = meta.per_page;
+    this.path = meta.path;
+  }
+}
---
+++ src/app/shared/abstracts/module-http.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ActionResponse } from '@fe-template/shared/models/responses/action.response';
+import { PaginatedResponse } from '@fe-template/shared/models/responses/paginated.response';
+import { HttpApiService } from '@fe-template/shared/service/http-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class ModuleHttpService<T = any, L = PaginatedResponse<T>, D = ActionResponse<T>> extends HttpApiService<T, L, D> {
+  protected abstract module: string;
+  protected abstract items: string;
+
+  protected getRoute(url: number | string | null = null): string {
+    if (!url) {
+      return `${ this.module }/${ this.items }`;
+    }
+    return `${ this.module }/${ this.items }/${ url }`;
+  }
+
+  public setModule(module: string): ModuleHttpService<T, L, D> {
+    this.module = module;
+
+    return this;
+  }
+
+  public setItems(items: string): ModuleHttpService<T, L, D> {
+    this.items = items;
+
+    return this;
+  }
+
+  protected getResourcePath(): string {
+    return this.getRoute();
+  }
+}
