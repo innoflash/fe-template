647e57b1c509f2b42fa02a3111ea6335d1faa1a7
innoflash <innocentmazando@gmail.com>
2022-04-25 12:16:42 +0200

    initialized http service

--- src/app/shared/abstracts/module-http.service.ts
+++ src/app/shared/abstracts/module-http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActionResponse } from '@fe-template/shared/models/responses/action.response';
 import { PaginatedResponse } from '@fe-template/shared/models/responses/paginated.response';
-import { HttpApiService } from '@fe-template/shared/service/http-api.service';
+import { HttpApiService } from '@fe-template/shared/services/http-api.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +9,6 @@ import { HttpApiService } from '@fe-template/shared/service/http-api.service';
 export abstract class ModuleHttpService<T = any, L = PaginatedResponse<T>, D = ActionResponse<T>> extends HttpApiService<T, L, D> {
   protected abstract module: string;
   protected abstract items: string;
-
-  protected getRoute(url: number | string | null = null): string {
-    if (!url) {
-      return `${ this.module }/${ this.items }`;
-    }
-    
-    return `${ this.module }/${ this.items }/${ url }`;
-  }
 
   public setModule(module: string): ModuleHttpService<T, L, D> {
     this.module = module;
@@ -28,6 +20,14 @@ export abstract class ModuleHttpService<T = any, L = PaginatedResponse<T>, D = A
     this.items = items;
 
     return this;
+  }
+
+  protected getRoute(url: number | string | null = null): string {
+    if (!url) {
+      return `${ this.module }/${ this.items }`;
+    }
+
+    return `${ this.module }/${ this.items }/${ url }`;
   }
 
   protected getResourcePath(): string {
